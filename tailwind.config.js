f59b155a129f7f10c5882bba840f84ff6f94980c
gaurabwebdev <gaurabbose40@gmail.com>
2023-07-19 21:17:14 +0600

    logo & menu done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-overlay":
+          "linear-gradient(to bottom right, #1A2C79 30%, rgba(26, 44,121, 0.18) 100%)",
+      },
+    },
   },
   plugins: [],
 };
